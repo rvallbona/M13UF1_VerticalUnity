d5cc410fa16b457ac0601c61566f4b7020098c45
Ramon Vallbona <98883990+rvallbona@users.noreply.github.com>
2023-05-25 19:14:11 +0200

    Cambiando camara, cambiando partículas

--- Assets/Scripts/Object/ThirdPersonCamera.cs
+++ Assets/Scripts/Object/ThirdPersonCamera.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class ThirdPersonCamera : MonoBehaviour
 {
     [SerializeField] Transform target;
-    [SerializeField] float distance = 12.0f;
-    [SerializeField] float distanceToIgnore = 10f;
+    [SerializeField] float distance;
 
     private float rotationX = 5.0f;
     private float rotationY = 5.0f;
     private Vector3 smoothVelocity = Vector3.zero;
 
-    [SerializeField] private float cameraLerp = 80f;
+    [SerializeField] private float cameraLerp;
 
     [SerializeField] Camera cam;
 
@@ -21,7 +20,7 @@ public class ThirdPersonCamera : MonoBehaviour
         rotationX += Input.GetAxis("Mouse Y");
         rotationY += Input.GetAxis("Mouse X");
 
-        rotationX = Mathf.Clamp(rotationX, -50f, 50f);
+        rotationX = Mathf.Clamp(rotationX, -30f, 30f);
 
         transform.eulerAngles = new Vector3(rotationX, rotationY, 0);
         Vector3 finalPosition = Vector3.Lerp(transform.position, target.transform.position -transform.forward * distance, cameraLerp * Time.deltaTime);
@@ -29,10 +28,6 @@ public class ThirdPersonCamera : MonoBehaviour
         RaycastHit hit;
         if (Physics.Linecast(target.transform.position, finalPosition, out hit))
         {
-            //if (hit.distance < distanceToIgnore)
-            //{
-            //    hit.transform.gameObject.layer = LayerMask.NameToLayer("Ignore Raycast");
-            //}
             finalPosition = hit.point;
             smoothVelocity = Vector3.zero;
         }
